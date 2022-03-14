2166a55de0abf5522c2540ade3a3525eb089ddae
Samuel Mensák <xmensak@fi.muni.cz>
2022-03-14 16:58:56 +0100

    Period picker prototype

--- Clever consumption/ViewController.swift
+++ Clever consumption/ViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var dayButton: UIButton!
+    @IBOutlet weak var weekButton: UIButton!
+    @IBOutlet weak var monthButton: UIButton!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var test: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        test.layer.cornerRadius = 10
+        dayButton.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+        dayButton.layer.cornerRadius = 10
+        monthButton.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        monthButton.layer.cornerRadius = 10
+
     }
+    
 
 
 }
